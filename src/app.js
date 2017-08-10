b6d2ab6ef592dbafce0c09a35a235a8cdcdef55a
JCamp-12 <username@users.noreply.github.com>
2017-08-10 19:21:46 -0400

    final update

--- src/app.js
+++ src/app.js
@@ -13,7 +13,43 @@ const readWords = () => {
   const contents = fs.readFileSync('words.txt', 'utf8');
   return contents.split('\n');
 };
-
+const words = readWords();
+const index = Math.floor(Math.random() * words.length);
+const guess = {};
+const word = words[index];
 // TODO: your code to handle requests
+
+server.post('/guess', (req, res) => {
+  const letter = req.body.letter;
+  if (!letter) {
+    res.status(STATUS_USER_ERROR);
+    res.json({ error: 'Must give letter' });
+    return;
+  }
+  if (letter.length !== 1) {
+    res.status(STATUS_USER_ERROR);
+    res.json({ error: 'Must give single letter' });
+    return;
+  }
+  if (guess[letter]) {
+    res.status(STATUS_USER_ERROR);
+    res.json({ error: 'Must give different letter' });
+    return;
+  }
+  guess[letter] = true;
+  res.json({ guess });
+});
+
+server.get('/', (req, res) => {
+  const hangman = word.split('').map((letter) => {
+    if (guess[letter]) {
+      return letter;
+    }
+    return '-';
+  })
+  .join('');
+  res.json({ hangman, guess });
+});
+
 
 server.listen(3000);
